bce376d7ff5e08fc3575a818e0db1e3ce6de0b10
sahitya <sahityakmr28@gmail.com>
2021-09-16 02:50:42 +0530

    Added Leetcode Solutions

--- platforms/leetcode/most-stone-removed-same-row-col.py
+++ platforms/leetcode/most-stone-removed-same-row-col.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class MostStoneRemoved:
-    def removeStone(self, stone: List[List[int]]) -> int:
+    def removeStones(self, stone: List[List[int]]) -> int:
         uf = {}
 
         def find(x):
@@ -11,5 +11,10 @@ class MostStoneRemoved:
             return uf[x]
 
         for i, j in stone:
-            uf[find(i)] = find(-j)
+            uf[find(i)] = find(~j)
         return len(stone) - len({find(x) for x in uf})
+
+
+#res = MostStoneRemoved().removeStones([[0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [2, 2]])
+res = MostStoneRemoved().removeStones([[0,1],[1,0]])
+print(res)
